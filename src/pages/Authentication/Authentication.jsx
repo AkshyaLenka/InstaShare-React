9ad5876cbd5887ab48a039bd7f05e268aecc563d
Akshya Lenka <akshya.lenka@comviva.com>
2024-09-29 00:42:48 +0530

    Integrated Login and Register API

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -1,21 +1,22 @@
-import { Card, Grid2 } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 import React from "react";
 import Login from "./Login";
 import Register from "./Register";
 
 const Authentication = () => {
   return (
-    <div className="relative h-screen">
-      <Grid2 container>
-        <Grid2 className="h-screen overflow-hidden" item xs={12}>
+    <div className="relative h-screen w-screen">
+      <Grid container>
+        {/* Background Image Grid */}
+        <Grid item xs={12} className="relative h-screen">
           <img
-            className="h-full w-full object-cover"
-            src="https://img.freepik.com/free-photo/elevated-view-various-mobile-icons-plywood_23-2147841381.jpg?t=st=1727519834~exp=1727523434~hmac=2c1cdf96b289d6f42853e45226ad351f97675bb1415eccc5c06652d67d61d899&w=740"
+            className="absolute top-0 left-0 h-full w-full object-cover"
+            src="https://img.freepik.com/premium-vector/beach-view-with-silhouettes-coconut-trees-shades-blue_621174-1719.jpg?w=826"
             alt="Instashare HomePage"
           />
-        </Grid2>
+        </Grid>
 
-        {/* Overlap the login box */}
+        {/* Card with Form */}
         <div className="absolute inset-y-0 right-0 flex items-center justify-center w-1/2 h-full px-20">
           <Card className="card p-8 w-full max-w-md">
             <div className="flex flex-col items-center mb-5 space-y-1">
@@ -24,11 +25,11 @@ const Authentication = () => {
                 Connecting People Seamlessly
               </p>
             </div>
-            {/* <Login /> */}
-            <Register />
+            <Login />
+            {/* <Register /> */}
           </Card>
         </div>
-      </Grid2>
+      </Grid>
     </div>
   );
 };
